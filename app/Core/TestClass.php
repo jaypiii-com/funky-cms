6c6aed8c9e3673ccf5afe508691eb74d03cadf43
binarios <behrens.de@icloud.com>
2025-03-10 15:15:51 +0100

    install composer and test autoloader with a Test class

---
+++ app/Core/TestClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FunkyCMS\Core;
+
+class TestClass
+{
+    public function __construct()
+    {
+        echo 'TestClass loaded';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+# JayPiii 2025 - FunkyCMS
+
+require __DIR__.'/../vendor/autoload.php';
+
+use FunkyCMS\Core\TestClass;
+
+(new TestClass());
